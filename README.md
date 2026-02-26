c114f038bd92e388fea90a7d6ff9cb5bafbe43bf
Ayla Myers <4943384+aylanonsense@users.noreply.github.com>
2026-02-26 15:05:15 -0500

    Tinkering

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Setup
+# Portfolio
 
-Run `npm run dev` to the development server then open [http://localhost:3000](http://localhost:3000) in your browser.
+## Setup
+
+Run `npm run dev` then open [http://localhost:3000](http://localhost:3000) in your browser.
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
+import Image from 'next/image';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>ayla~nonsense</h1>
-    </main>
+    <div className={styles.content}>
+      <header>
+        <h1>ayla~nonsense</h1>
+        <p>Game developer</p>
+      </header>
+      <main>
+        <section className={styles.games}>
+          <p>Games</p>
+          <section className={styles["big-games"]}>
+            <Image
+              src="/beast-breaker.avif"
+              width={800}
+              height={500}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/clone-drone-in-the-hyperdome.jpg"
+              width={800}
+              height={500}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/lost-your-marbles.jpg"
+              width={800}
+              height={500}
+              alt="Picture of the author"
+            />
+          </section>
+          <section>
+            <h2>Small games</h2>
+          </section>
+          <section>
+            <h2>Mini games</h2>
+          </section>
+        </section>
+        <section>
+          <p>Resume</p>
+        </section>
+      </main>
+    </div>
   );
 }
