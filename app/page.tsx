783e8b2467901ccc03c0650ec15fca5bf0584a65
ayla~nonsense <4943384+aylanonsense@users.noreply.github.com>
2026-03-05 16:13:50 -0500

    Beginning to add optimized gameplay gifs

--- app/page.tsx
+++ app/page.tsx
@@ -31,41 +31,101 @@ export default function Home() {
                 src="/Lost Your Marbles title art.webp"
                 width={376}
                 height={500}
-                alt="x"
+                alt="Lost Your Marbles"
               />
             </li>
             <li className={styles.big}>
               <Image
                 src="/Just One Boss gameplay.gif"
                 className={styles.pixelated}
-                width={375}
-                height={375}
-                alt="x"
-              />
-            </li>
-            <li className={styles.big}>
-              <Image
-                src="/8 Legs to Love gameplay.gif"
-                className={styles.pixelated}
-                width={375}
-                height={375}
-                alt="x"
+                width={128}
+                height={128}
+                alt="Just One Boss"
               />
             </li>
             <li className={styles.big}>
               <Image
                 src="/arpongi gameplay.gif"
                 className={styles.pixelated}
-                width={375}
-                height={375}
-                alt="x"
+                width={127}
+                height={127}
+                alt="arpongi"
+              />
+            </li>
+            <li className={styles.big}>
+              <Image
+                src="/8 Legs to Love gameplay.gif"
+                className={styles.pixelated}
+                width={128}
+                height={128}
+                alt="8 Legs to Love"
+              />
+            </li>
+            <li className={styles.big}>
+              <Image
+                src="/staring gameplay.gif"
+                className={styles.pixelated}
+                width={190}
+                height={190}
+                alt="staring"
               />
             </li>
             <li className={styles.big}></li>
             <li className={styles.big}></li>
-            <li className={styles.big}></li>
-            <li className={styles.medium}></li>
-            <li className={styles.medium}></li>
+            <li className={styles.medium}>
+              <Image
+                src="/Quickdraw Blackjack gameplay.gif"
+                className={styles.pixelated}
+                width={190}
+                height={190}
+                alt="Quickdraw Blackjack"
+              />
+            </li>
+            <li className={styles.medium}>
+              <Image
+                src="/AGAB gameplay.gif"
+                className={styles.pixelated}
+                width={128}
+                height={128}
+                alt="AGAB"
+              />
+            </li>
+            <li className={styles.medium}>
+              <Image
+                src="/Snowball Showdown gameplay.gif"
+                className={styles.pixelated}
+                width={127}
+                height={127}
+                alt="Snowball Showdown"
+              />
+            </li>
+            <li className={styles.medium}>
+              <Image
+                src="/Recommendation Dog gameplay.gif"
+                className={styles.pixelated}
+                width={280}
+                height={280}
+                alt="Recommendation Dog"
+              />
+            </li>
+            <li className={styles.medium}>
+              <Image
+                src="/Juggle Panic gameplay.gif"
+                className={styles.pixelated}
+                width={126}
+                height={126}
+                alt="Juggle Panic"
+              />
+            </li>
+            <li className={styles.medium}>
+              <Image
+                src="/Reel Steal gameplay.gif"
+                className={styles.pixelated}
+                width={240}
+                height={240}
+                alt="Reel Steal"
+              />
+            </li>
             <li className={styles.medium}></li>
             <li className={styles.medium}></li>
             <li className={styles.medium}></li>
