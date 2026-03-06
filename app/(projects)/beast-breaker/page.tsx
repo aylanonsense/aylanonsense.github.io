a035f6b1a7af8e87ef11e2e8c2ceb6c1057e394a
Ayla Myers <4943384+aylanonsense@users.noreply.github.com>
2026-03-06 16:05:36 -0500

    More icons

--- app/(projects)/beast-breaker/page.tsx
+++ app/(projects)/beast-breaker/page.tsx
@@ -12,15 +12,6 @@ export default function Page() {
       <p>Released September 2021</p>
       <p><a href="https://store.epicgames.com/en-US/p/beast-breaker" target="_blank" rel="noopener noreferrer">Switch</a></p>
       <p><a href="https://www.nintendo.com/us/store/products/beast-breaker-switch/" target="_blank" rel="noopener noreferrer">Epic Games</a></p>
-      <Icon site="Epic Games" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="Facebook" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="GitHub" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="Instagram" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="itch.io" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="Nintendo Switch" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="Twitch" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="Twitter" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
-      <Icon site="YouTube" width={50} height={50} style={{ fill: "white", background: "black", border: "1px solid black" }}/>
     </>
   );
 }
