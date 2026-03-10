92d48dfb142fb4f26f53b33a0a9df45c26e73f0b
ayla~nonsense <4943384+aylanonsense@users.noreply.github.com>
2026-03-10 16:02:43 -0400

    Iterating on the Clone Drone in the Hyperdome page

--- app/(projects)/clone-drone-in-the-hyperdome/page.tsx
+++ app/(projects)/clone-drone-in-the-hyperdome/page.tsx
@@ -1,21 +1,30 @@
+import ProjectContent from "@/app/components/ProjectContent";
+import ProjectHeader from "@/app/components/ProjectHeader";
 import YouTubeEmbed from "@/app/components/YouTubeEmbed";
 
 export default function Page() {
   return (
     <>
-      <p>A turn-based action adventure combining traditional RPG mechanics with Peggle-like bouncing and ricochets. You play as Skipper, a small mouse tasked with defending a cast of charming characters from mosaic Beasts. The core gameplay revolves around battling Beasts, crafting/upgrading equipment, conversing with your companions, and uncovering the overarching narrative over the course of a 15-hour playthrough.</p>
-      <h2>Clone Drone in the Hyperdome</h2>
-      <YouTubeEmbed
-        src="https://www.youtube-nocookie.com/embed/Lak9cQqVsrM?si=XFuV7R1CJdw9pcjK"
-        width={560}
-        height={315} />
-      <p>Clone Drone in the Hyperdome is a virtual reality laser sword fighting game with voxel destruction and physics-based combat. Players participate in runs of the Crashloop to gain powerful weapons and super-powered gauntlets called Turbofists in the hopes of defeating the Captain in the Hyperdome.</p>
-      <p>CDHD released to Very Positive reviews on <a href="https://store.steampowered.com/app/2401230/Clone_Drone_in_the_Hyperdome/" target="_blank" rel="noopener noreferrer">Steam</a> and 4.6 stars on the <a href="https://www.meta.com/experiences/clone-drone-in-the-hyperdome/4523126804481573/" target="_blank" rel="noopener noreferrer">Meta Store</a> on December 12, 2024.</p>
-      <YouTubeEmbed
-        src="https://www.youtube-nocookie.com/embed/xwJryg1nNfc?si=0uKtyJXYUo0-o_5G"
-        width={560}
-        height={315} />
-      <p>On July 27, 2025 a free update to CDHD was released which introduced the Skyscrapper, an endless tower of robot carnage.</p>
+      <ProjectHeader
+        title="Clone Drone in the Hyperdome"
+        releaseDate="December 12, 2024"
+        links={[
+          "https://store.steampowered.com/app/2401230/Clone_Drone_in_the_Hyperdome/",
+          "https://www.meta.com/experiences/clone-drone-in-the-hyperdome/4523126804481573/",
+        ]}>
+        <YouTubeEmbed
+          src="https://www.youtube-nocookie.com/embed/Lak9cQqVsrM?si=XFuV7R1CJdw9pcjK"
+          aspectRatio={560 / 315} />
+      </ProjectHeader>
+      <ProjectContent>
+        <p>A virtual reality laser sword fighting game with voxel destruction and physics-based combat. Players participate in runs of the Crashloop to gain powerful weapons and super-powered gauntlets called Turbofists in the hopes of defeating the Captain in the Hyperdome.</p>
+        <p>CDHD released to Very Positive reviews on <a href="https://store.steampowered.com/app/2401230/Clone_Drone_in_the_Hyperdome/" target="_blank" rel="noopener noreferrer">Steam</a> and 4.6 stars on the <a href="https://www.meta.com/experiences/clone-drone-in-the-hyperdome/4523126804481573/" target="_blank" rel="noopener noreferrer">Meta Store</a> on December 12, 2024.</p>
+        <p>On July 27, 2025 a free update to CDHD was released which introduced the Skyscrapper, an endless tower of robot carnage.</p>
+        <YouTubeEmbed
+          src="https://www.youtube-nocookie.com/embed/xwJryg1nNfc?si=0uKtyJXYUo0-o_5G"
+          width={560}
+          height={315} />
+      </ProjectContent>
     </>
   );
 }
