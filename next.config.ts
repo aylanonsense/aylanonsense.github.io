cea021d90b6d4f6838db22efa49569c780dcf548
Ayla Myers <4943384+aylanonsense@users.noreply.github.com>
2026-03-30 01:55:30 -0400

    Set up Next.js to export as static assets

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import path from "path"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: path.join(__dirname, '.'),
   },
